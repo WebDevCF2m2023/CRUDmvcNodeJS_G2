672edf9dd465e62d209ee5d883db1b3a1703bfa8
CF2Mweb_PO <pierre.sandron@cf2m.be>
2024-10-10 15:36:02 +0200

    Ajouter méthodes de connexion

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -115,12 +115,77 @@ exports.register = (req,res) => {
 
 // Pour afficher le formulaire de connexion de l'utilisateur
 exports.login_form = (req,res) => {
-    
+    console.log("user.controller .login_form : req.session = "+req.session);
+
+    const {userId} = req.session;
+    console.log("user.controller .login_form : userID = "+userId);
+
+    // Si une session est déjà active, on affiche la page d'accueil pour un utilisateur déjà loggé, car pas besoin de se connecter
+    if (userId) {
+        console.log("Déjà loggé - userID="+userId);
+        User.getUserById(userId,(err,utilisateur)=>{
+            if (err) {
+                res.status(500).send({
+                    title:'Erreur 500',
+                    message:"Erreur, dans loginform, pendant la recherche d'un utilisateur sur base de son ID"
+                });
+            } else {
+                // Aller sur l'accueil "connecté"
+                res.render('index_admin.ejs',{title:'Express - Admin',user:utilisateur.nom});
+            }
+        });
+    } else {
+    // Sinon, on affiche le formulaire de connexion  
+        console.log("Affichage du formulaire de connexion");
+        res.render("login_form.ejs", {title:"Connexion"});
+    }
 };
 
 // Pour vérifier que l'utilisateur est correct (login/mot de passe OK)
 exports.login = (req,res) => {
-    
+    console.log("user.controller - .login");
+    const lemail = req.body.email;
+    let lemotdepasse = req.body.motdepasse;
+
+    // valider le contenu du formulaire
+    if ((!req.body)||(lemail==="")||(lemotdepasse==="")) {
+        console.log("Le contenu du formulaire n'est pas valide !");
+        res.render("login_form.ejs",{title:"Connexion"});
+    } else {
+        console.log(req.body);
+
+        // Rechercher si l'e-mail n'est pas déjà enregistré
+        User.getUserByEmail(lemail, (err,data)=>{
+            console.log(`User.getUserByEmail : ${err}`);
+            if (err) {
+                console.log("Email inconnu");
+                if (err.type === "ERR_NOT_FOUND"){
+                    // E-mail inconnu, on renvoie vers le formulaire d'enregistrement
+                    res.render('register_form.ejs',{title:'Enregistrement'});
+                } else {
+                    res.status(500).send({
+                        title: "Erreur 500", message: "Erreur (getUserByEmail) pendant l'enregistrement d'un utilisateur", error:err
+                    });
+                }
+            } else {
+                console.log("Email connu - Données : "+data);
+                console.log(`Mot de passe encodé = ${lemotdepasse}`);
+                console.log(`Mot de passe attendu = ${utilisateur.motdepasse}`);
+
+                const isValidPassword = compareSync(lemotdepasse,utilisateur.motdepasse);
+
+                if (isValidPassword) {
+                    console.log("Mot de passe OK, on connecte l'utilisateur");
+                    utilisateur.motdepasse = undefined;
+                    req.session.userId = utilisateur.id;
+                    res.render("index_admin.ejs",{title:'Express - Admin', user: utilisateur.nom});
+                } else {
+                    console.log("Mauvais mot de passe, retour à l'écran de connexion");
+                    res.render("login_form.ejs",{title:"Connexion"});
+                }
+            }
+        });
+    }
 };
 
 // Pour se déconnecter
