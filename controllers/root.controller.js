bfbc7753e7cc73d37c51c33b6e048bed5ece9fd2
CF2Mweb_PO <pierre.sandron@cf2m.be>
2024-10-14 09:57:47 +0200

    Formulaire message sécurisé

--- controllers/root.controller.js
+++ controllers/root.controller.js
@@ -22,17 +22,39 @@ function securiserChamp(donnees) {
     return donnees;
 }
 
+// Route pour l'accueil
 exports.home = (req, res, next) => {
-    res.render('index.ejs', { title: 'Express' });
+    const { userId } = req.session;
+    console.log("root.controller .home - userId = ",userId);
+
+    // Si une session est déjà active, on affiche l'accueil pour utilisateur loggé
+    if (userId) {
+        // rechercher l'utilisateur
+        User.getUserById(userId,(err,utilisateur)=>{
+            if (err){
+                res.status(500).send({
+                    title: "Erreur 500",
+                    message: "Erreur dans getUserById "
+                });
+            } else {
+                res.render('index_admin.ejs', {title:'Express - Admin', user: utilisateur.nom })
+            }
+        });
+    } else { // Sinon, on affiche l'accueil pour utilisateur non loggé
+        res.render('index.ejs', { title: 'Express' });
+    }
 };
 
+// Route pour afficher le formulaire de création d'un message
 exports.form = (req, res, next) => {
     res.render('contact_form.ejs',{ title: 'Formulaire de contact' });
 };
 
+// Route pour afficher les données encodées
 exports.traitement = (req, res, next) => {
-    let lenom = req.body.nom;
-    let lemessage = req.body.msg;
+    // récupération dans le formulaire du nom et du message, avec sécurisation
+    let lenom = securiserChamp(req.body.nom);
+    let lemessage = securiserChamp(req.body.msg);
 
     res.render('traiter_form.ejs', { title: "Formulaire reçu", nom:lenom, msg: lemessage});
 };
